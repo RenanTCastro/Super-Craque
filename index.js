6c39d3766bdd51e7fd39b54f3f35de2f82e18f73
RenanTCastro <renancastrotut@gmail.com>
2024-02-24 16:18:42 -0300

    feat: criado front da tela de partidas

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.get("/screens", (req, res) => {
+  res.sendFile("./public/screens/match");
+});
+
 app.listen(8001, () => {
-    console.log("Estou escutando na porta É MAIS DE 8 mil! (8001)");
+  console.log("Estou escutando na porta É MAIS DE 8 mil! (8001)");
 });
